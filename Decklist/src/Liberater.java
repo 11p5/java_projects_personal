342a383122bdefc817614f3265e404be15ed97f6
11p5 <ps392@student.le.ac.uk>
2018-01-14 15:43:11 +0000

    the first update the add decklist

---
+++ Decklist/src/Liberater.java
@@ -0,0 +1,19 @@
+
+public class Liberater {
+	
+	public static void main (String[]arg) {
+	String[] Lib;
+	
+	Lib = new String[360];
+	
+	String[] Lib2 = {"Liberator, Lucky Charmy x4 ","Plodmy Liberator x4","Liberator of Hope, Epona" };
+	System.out.print("Liberator deck "+Lib2);
+	
+	
+	
+	if((int i == 0,i<361, i++) ) {
+		
+	}
+	
+	}
+}
